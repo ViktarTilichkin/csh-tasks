6d0bf7ba650ad3d712efbc7c79dd53d91f7c4c24
ViktarTilichkin <tilichkin@mail.ru>
2022-10-14 21:15:50 +0300

    add test-tasks

---
+++ test-tasks/ConsoleApplication/Program.cs
@@ -0,0 +1,9 @@
+﻿// Console.WriteLine("Hello, " "World!");
+Console.WriteLine("Hello, " + "World!");
+// Console.WriteLine('Hello, World!");
+Console.WriteLine("Hello, World!");
+// Console.WriteLine(Hello, World!);
+Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, {name}!);
+var name = Console.ReadLine();
+Console.WriteLine($"Hello, {name}!");
