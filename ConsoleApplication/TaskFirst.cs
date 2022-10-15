134b1437bc85b680674bf58d730a0b5d859d9daf
ViktarTilichkin <tilichkin@mail.ru>
2022-10-15 18:56:12 +0300

    clear

--- ConsoleApplication/TaskFirst.cs
+++
@@ -1,9 +0,0 @@
-﻿// Console.WriteLine("Hello, " "World!");
-Console.WriteLine("Hello, " + "World!");
-// Console.WriteLine('Hello, World!");
-Console.WriteLine("Hello, World!");
-// Console.WriteLine(Hello, World!);
-Console.WriteLine("Hello, World!");
-// Console.WriteLine("Hello, {name}!);
-var name = Console.ReadLine();
-Console.WriteLine($"Hello, {name}!");
