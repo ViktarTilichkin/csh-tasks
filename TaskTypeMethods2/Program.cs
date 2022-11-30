11dd37ebe7fae4ddbc3978ffd3d48328e02b989c
ViktarTilichkin <tilichkin@mail.ru>
2022-11-30 23:17:43 +0300

    update Methods2

--- TaskTypeMethods2/Program.cs
+++ TaskTypeMethods2/Program.cs
@@ -4,21 +4,90 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        // Task1();
+        Task2();
     }
 
-    
+
     #region Задача1
     // 1. На входе дано три массива заполненных случайными числами. Написать метод
     // по подсчёту общего количества чётных чисел в массивах. (ref)
-    
+    // static void Task1()
+    // {
+    //     int[] arrayNumber = { 15, 58, 16, 819, 156, 651, 1568 };
+    //     int[] arrauNumber2 = { 1649, 13, 15, 17, 16, 18, 25, 16 };
+    //     int[] arrauNumber3 = { 1651, 6548, 1989, 2165, 46165, 121, 21, 64, 655, 564 };
+    //     int result = EvenNumbersArray(arrayNumber, arrauNumber2, arrauNumber3);
+
+    // }
+    // static int EvenNumbersArray(params int[] array)
+    // {
+    //     return 50;
+    // }
 
     #endregion
     #region Задача 2
     // 2. Написать метод, который проверяет является ли введнная строка почтовым
     // адрессом и возвращающий домен почты.(out)
-    #endregion
+    static void Task2()
+    {
+        Console.WriteLine("задача 2");
+        Console.WriteLine("Введите email");
+        string? input = Console.ReadLine();
+        if (!string.IsNullOrEmpty(input))
+        {
+            Console.WriteLine(ChekEmail(input, out string domen));
+        }
+        else
+        {
+            Console.WriteLine("error");
+        }
+    }
+    static bool ChekEmail(string email, out string domen)
+    {
+        domen = "";
+        email = email.Trim();
+        bool resultEmail = false;
+        int symbol1 = 0;
+        for (int i = email.Length - 1; i > -1; i--)
+        {
+            if (!resultEmail && email[i] != '.' && email[i] != '@')
+            {
+                domen += email[i];
+            }
+            else if (email[i] == '.')
+            {
+                domen += email[i];
+                symbol1++;
+            }
+            else if (email[i] == '@')
+            {
+                domen += email[i];
+                break;
+            }
+        }
+        for (int i = 0; i < email.Length; i++)
+        {
+            if (email[i] == ' ')
+            {
+                symbol1 = 2;
+            }
+        }
+        if (symbol1 == 1)
+        {
+            resultEmail = true;
+            string result = new string(domen.ToCharArray().Reverse().ToArray());
+            Console.WriteLine($"Email domen {result}");
+            return resultEmail;
+        }
+        else
+        {
+            Console.WriteLine(domen = "is not email");
+            return resultEmail;
+        }
+    }
 
+    #endregion
     #region Задача 3
     // 3. Написать метод, который удаляет из строки заданный символ. Символ является
     // входным параметром-значением. Строка должна быть параметром-ссылкой и
