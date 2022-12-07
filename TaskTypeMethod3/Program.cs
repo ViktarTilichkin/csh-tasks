c62346570396bb34aeea766d9635d6bbf484c032
ViktarTilichkin <tilichkin@mail.ru>
2022-12-07 22:17:57 +0300

    update Methods3 3/4

--- TaskTypeMethod3/Program.cs
+++ TaskTypeMethod3/Program.cs
@@ -3,10 +3,10 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Task1();
-        Task2();
+        // Task1();
+        // Task2();
         Task3();
-        Task4();
+        // Task4();
     }
     #region Задача 1
     //     1. Написать методы с ипользованием лямбда-оператора по выполнению
@@ -48,11 +48,53 @@ internal class Program
     // участка, в виде HH:MM:SS, длинну учатска контроля скорости и среднюю
     // допустимую скорость участка
     // В задаче использовать необязательные парметры, локальные методы.
-     static void Task2()
+    static void Task2()
     {
         Console.WriteLine("Задача 2");
-        string[] text = new string[0];
+        Console.WriteLine("Введите время старта HH:MM:SS");
+        string? input = Console.ReadLine();
+        Console.WriteLine("Введите время финиша HH:MM:SS");
+        string? input2 = Console.ReadLine();
+        if (!string.IsNullOrEmpty(input) && !string.IsNullOrEmpty(input2))
+        {
+            MetSpeed(input, input2, roadLength: 5);
+        }
+        else
+        {
+            Console.WriteLine("error input");
+        }
+    }
+    static void MetSpeed(string timeStart, string timeFinish, double roadLength = 1, double topspeed = 90)
+    {
+        double startT = convertorTime(timeStart);
+        double finishT = convertorTime(timeFinish);
+        if (startT < finishT)
+        {
+            double averageSpeed = roadLength / (finishT - startT);
+            if (averageSpeed > (topspeed + 10))
+            {
+                Console.WriteLine($"Водитель превысел скорость на {averageSpeed - topspeed}");
+            }
+            else
+            {
+                Console.WriteLine($"водитель не нарушал, средняя скорость {averageSpeed}, разрешенная {topspeed}");
 
+            }
+        }
+        else
+        {
+            Console.WriteLine("error input");
+        }
+        double convertorTime(string time)
+        {
+            time.Trim();
+            string[] arrayTime = time.Split(":");
+            double hour = Convert.ToDouble(arrayTime[0]);
+            double minuts = Convert.ToDouble(arrayTime[1]);
+            double second = Convert.ToDouble(arrayTime[2]);
+            double result = hour + minuts / 60 + second / 3600;
+            return result;
+        }
     }
 
     #endregion
@@ -62,34 +104,50 @@ internal class Program
     static void Task3()
     {
         Console.WriteLine("Задача 3");
-        string[] text = new string[0];
+        string[] text = { "new string[0]" };
+        Console.WriteLine(string.Join(", ", text));
+        object[] array = MetArrayConvert(text, 10, "string");
+        Console.WriteLine(string.Join(", ", array));
+    }
 
+    static object[] MetArrayConvert(object[] array, int addlength = 1, string def = "0")
+    {
+        object[] newArray = new object[array.Length + addlength];
+        Array.Copy(array, newArray, array.Length);
+        for (int i = array.Length; i < newArray.Length; i++)
+        {
+            newArray[i] = def;
+            Console.WriteLine(newArray[i]);
+        }
+        array = new object[array.Length + addlength];
+        Array.Copy(newArray, array, newArray.Length);
+        return array;
     }
     #endregion
 
     #region Задача 4
     // 4. Написать метод по расчету корней квадратного уравнения с использованием
     // локального метода
-    static void Task4()
-    {
-        Console.WriteLine("Задача 4");
-        Console.WriteLine("Введите число: ");
-        string? input = Console.ReadLine();
-        if (double.TryParse(input, out double number))
-        {
-            
-        }
-        else
-        {
-            Console.WriteLine("error");
-        }
+    // static void Task4()
+    // {
+    //     Console.WriteLine("Задача 4");
+    //     Console.WriteLine("Введите число: ");
+    //     string? input = Console.ReadLine();
+    //     if (double.TryParse(input, out double number))
+    //     {
 
-    }
-    static double MetSquare(double number)
-    {
-        
+    //     }
+    //     else
+    //     {
+    //         Console.WriteLine("error");
+    //     }
 
-    }
+    // }
+    // static double MetSquare(double number)
+    // {
+
+
+    // }
 
     #endregion
 }
